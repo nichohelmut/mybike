71aba2253f75e65948d9a19e93c2e40fa6c87fe2
nicholasutikal <nicholasutikal@gmail.com>
2022-01-06 17:13:27 +0100

    adds item picker

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import React from "react";
-
-import AppText from "./app/components/AppButton";
-
-import { ImageBackground, StyleSheet, View, Image } from "react-native";
-import AppButton from "./app/components/AppButton";
-import ListingDetailScreen from "./app/screens/ListingDetailScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AppCard from "./app/components/AppCard";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import AccountScreen from "./app/screens/AccountScreen";
+import React, { useState } from "react";
 import Screen from "./app/components/Screen";
-import AccountItem from "./app/components/AccountItem";
-import ListItem from "./app/components/ListItem";
-import ListingsScreen from "./app/screens/ListingsScreen";
+
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furnituer", value: 1 },
+  { label: "Cloth", value: 2 },
+  { label: "Macs", value: 3 },
+];
 
 export default function App() {
-  return <ListingsScreen />;
+  const [category, setCategory] = useState(categories[0]);
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      ></AppPicker>
+      <AppTextInput icon="email" placeholder="Email"></AppTextInput>
+    </Screen>
+  );
 }
