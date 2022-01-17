64f31c23a48591a3b6805297ca1dc8f793b90ada
nicholasutikal <nicholasutikal@gmail.com>
2022-01-17 11:39:32 +0100

    refactors forms

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
-import Screen from "./app/components/Screen";
 
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
+import LoginScreen from "./app/screens/LoginScreen";
 
-const categories = [
-  { label: "Furnituer", value: 1 },
-  { label: "Cloth", value: 2 },
-  { label: "Macs", value: 3 },
-];
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
+
   return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      ></AppPicker>
-      <AppTextInput icon="email" placeholder="Email"></AppTextInput>
-    </Screen>
+  <LoginScreen />
   );
 }
