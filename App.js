27bab26c23efa6c4db5ff40ba636067614fef24f
nicholasutikal <nicholasutikal@gmail.com>
2022-02-18 11:08:03 +0100

    adds basic image selector

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import ListingDetailScreen from "./app/screens/ListingDetailScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import * as ImagePicker from "expo-image-picker";
+import { Button, Image } from "react-native";
+
 export default function App() {
+  const [imageUri, setImageUri] = useState();
 
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library.");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
